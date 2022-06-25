90e0c9e089714cdf75cf71ecab89c3cf53533005
tecnopuc <tecnopuc@i4055083.local>
2022-06-25 17:17:13 -0300

    first commit

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,44 @@
-import { Fitrash2, FiPlus } from 'react-icons/fi'
+import { useState } from 'react'
+import { FiTrash2, FiPlus } from 'react-icons/fi'
 
 import './list.styles.css'
+
 const List = () => {
-    return(
+  const[list, setList] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  function handleCreateNewTask() {
+    const task = {
+      id: Math.random(),
+      title: newTask,
+      isComplete: false
+    }
+
+    if(task.title === '') {
+        return
+    }
+
+    setList([...list, task])
+    setNewTask('')
+  }
+
+  function handleToggleTaskCompletion(id){
+    const newTasks = list.map(task => task.id === id? {
+      ...task,
+      isComplete: !task.isComplete,
+    }:task)
+
+    setList(newTasks)
+  }
+
+function handleDeleteTask(id) {
+    const tasksFiltered = list.filter(task => task.id !== id)
+    setList(tasksFiltered)
+}
+
+
+
+return(
 <section className='list'>
 <header>
 <h2>Tarefas a fazer:</h2>
@@ -11,15 +47,42 @@ const List = () => {
     <input 
      type='text'
      placeholder='Adicionar nova tarefa'
-     />
-     <button className= 'add-task'>
-     <FiPlus size= {16} color='#FFFAFA'/>
+     onChange= {e => setNewTask(e.target.value)}
+     value={newTask}
+    />
+     <button className= 'add-task' data-test-id='add-task' onClick={handleCreateNewTask}>
+     <FiPlus size={16} color='#FFFAFA'/>
      </button>
-</div>
+   </div>
 </header>
+
+<main>
+    <ul>
+        {
+            list.map(task => 
+                <li key={task.id}>
+            <div data-testid='task' className={task.isComplete? 'completed': ''}>
+                <label className={'checkbox-container'}>
+                    <input 
+                     type='checkbox'
+                     onClick= {() => handleToggleTaskCompletion(task.id)}
+                     checked={task.isComplete} 
+                     readOnly
+                    />
+                    <span className='checkmark'></span>
+                </label>
+                <p>{task.title}</p>
+            </div>
+            <button className='remove-task' data-testid='remove-task' onClick={() => handleDeleteTask(task.id)} >
+                <FiTrash2 size={16} />
+            </button>
+        </li>
+                )
+        }
+    </ul>
+</main>
 </section>
     )
 }
-
-
+ 
 export default List
